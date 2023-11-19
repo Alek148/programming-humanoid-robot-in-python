d31787e91af610f5ae8b63227fbebd950d470d4f
Aleksander Garlikowski <alek.garlikowski@gmail.com>
2023-11-19 22:28:55 +0100

    Moved standing_up implementation to the correct class

--- joint_control/standing_up.py
+++ joint_control/standing_up.py
@@ -11,39 +11,19 @@ from keyframes import hello, rightBackToStand, rightBellyToStand, wipe_forehead
 from time import time
 
 class StandingUpAgent(PostureRecognitionAgent):
+
+    def __init__(self, simspark_ip='localhost', simspark_port=3100, teamname='DAInamite', player_id=0, sync_mode=True):
+        super().__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.last_posture = None
+
     def think(self, perception):
         self.standing_up()
         return super(StandingUpAgent, self).think(perception)
 
     def standing_up(self):
         posture = self.posture
-        print(posture)
+        #print(posture)
         # YOUR CODE HERE
-
-
-class TestStandingUpAgent(StandingUpAgent):
-    '''this agent turns off all motor to falls down in fixed cycles
-    '''
-    def __init__(self, simspark_ip='localhost',
-                 simspark_port=3100,
-                 teamname='DAInamite',
-                 player_id=0,
-                 sync_mode=True):
-        super(TestStandingUpAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
-        self.stiffness_on_off_time = 0
-        self.stiffness_on_cycle = 10  # in seconds
-        self.stiffness_off_cycle = 3  # in seconds
-        self.last_posture = None
-
-    def think(self, perception):
-        action = super(TestStandingUpAgent, self).think(perception)
-        time_now = perception.time
-        if time_now - self.stiffness_on_off_time < self.stiffness_off_cycle:
-            action.stiffness = {j: 0 for j in self.joint_names}  # turn off joints
-        else:
-            action.stiffness = {j: 1 for j in self.joint_names}  # turn on joints
-        if time_now - self.stiffness_on_off_time > self.stiffness_on_cycle + self.stiffness_off_cycle:
-            self.stiffness_on_off_time = time_now
 
         if time() - self.time_begin >= 10: # I could make a proper test for keyframe animation ending, but this already works
             self.last_posture = None
@@ -62,8 +42,31 @@ class TestStandingUpAgent(StandingUpAgent):
             self.keyframes = wipe_forehead(None)
             self.begin_motion()
 
-        return action
 
+class TestStandingUpAgent(StandingUpAgent):
+    '''this agent turns off all motor to falls down in fixed cycles
+    '''
+    def __init__(self, simspark_ip='localhost',
+                 simspark_port=3100,
+                 teamname='DAInamite',
+                 player_id=0,
+                 sync_mode=True):
+        super(TestStandingUpAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.stiffness_on_off_time = 0
+        self.stiffness_on_cycle = 10  # in seconds
+        self.stiffness_off_cycle = 3  # in seconds
+
+    def think(self, perception):
+        action = super(TestStandingUpAgent, self).think(perception)
+        time_now = perception.time
+        if time_now - self.stiffness_on_off_time < self.stiffness_off_cycle:
+            action.stiffness = {j: 0 for j in self.joint_names}  # turn off joints
+        else:
+            action.stiffness = {j: 1 for j in self.joint_names}  # turn on joints
+        if time_now - self.stiffness_on_off_time > self.stiffness_on_cycle + self.stiffness_off_cycle:
+            self.stiffness_on_off_time = time_now
+
+        return action
 
 if __name__ == '__main__':
     agent = TestStandingUpAgent()
